f4aaafedbefe04a74183f97c6b83120bf9324247
Istiaque-Hossain <istiaque842@gmail.com>
2023-04-18 11:43:11 +0600

    submit

--- module-11/index.php
+++ module-11/index.php
@@ -16,8 +16,8 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Input the Number" aria-label="Input the Number" aria-describedby="button-addon2">
-                    <button onclick="multiplicationTable()" class="btn btn-outline-secondary" type="button" id="button-addon2">Check</button>
+                    <input type="text" class="form-control" placeholder="Input the Number" aria-label="Input the Number" aria-describedby="button-addon2" id="my-input">
+                    <button onclick="num_multiplicationTable()" class="btn btn-outline-secondary" type="button" id="button-addon2">Check</button>
                 </div>
             </div>
 
@@ -27,8 +27,19 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
     <script>
+        function num_multiplicationTable() {
+            let num = document.getElementById('my-input').value;
+            multiplicationTable(num);
+        }
+
         function multiplicationTable(num) {
-            console.log('clicked');
+            let i = 1;
+            while (i <= 10) {
+                product = num * i;
+                console.log(num + 'x' + i + ' = ' + product);
+                i++;
+            }
+
         }
     </script>
 </body>
